9b1b754bdc0fbb8e772629f09b5c6ee1974a765b
rajkhare <rajkhare@192.168.0.105>
2020-08-29 15:54:08 +0530

    TEST

--- learncamel-simple-route/src/main/java/com/learncamel/file/CopyFilesRoute.java
+++ learncamel-simple-route/src/main/java/com/learncamel/file/CopyFilesRoute.java
@@ -1,15 +1,14 @@
 package com.learncamel.file;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.model.RouteDefinition;
 
 public class CopyFilesRoute extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
-		RouteDefinition definition = from("file:data/input?noop=true")
-		    .to("log:?level=INFO&showHeader=true&showBody=true")
-		    .to("file:data/output");
+		from("file:data/input?noop=true")
+	    .to("log:?level=INFO&showBody=true&showHeaders=true")
+	    .to("file:data/output");
 		
 	}
 
